9016a23c30f1c655f7a743434481050e7bd5a6c3
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-16 16:34:13 +0300

    HourlyWeather is Parcelable now

--- app/src/main/java/com/tatar/stormy/model/HourlyWeather.java
+++ app/src/main/java/com/tatar/stormy/model/HourlyWeather.java
@@ -1,16 +1,39 @@
 package com.tatar.stormy.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by emuoztu on 7/12/2017.
  */
 
-public class HourlyWeather {
+public class HourlyWeather implements Parcelable {
 
     private long time;
     private String summary;
     private double temperature;
     private String icon;
     private String timeZone;
+
+    protected HourlyWeather(Parcel in) {
+        time = in.readLong();
+        summary = in.readString();
+        temperature = in.readDouble();
+        icon = in.readString();
+        timeZone = in.readString();
+    }
+
+    public static final Creator<HourlyWeather> CREATOR = new Creator<HourlyWeather>() {
+        @Override
+        public HourlyWeather createFromParcel(Parcel in) {
+            return new HourlyWeather(in);
+        }
+
+        @Override
+        public HourlyWeather[] newArray(int size) {
+            return new HourlyWeather[size];
+        }
+    };
 
     public long getTime() {
         return time;
@@ -50,5 +73,19 @@ public class HourlyWeather {
 
     public void setTimeZone(String timeZone) {
         this.timeZone = timeZone;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(time);
+        dest.writeString(summary);
+        dest.writeDouble(temperature);
+        dest.writeString(icon);
+        dest.writeString(timeZone);
     }
 }
