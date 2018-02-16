f852bdcdedee0af122c7a28d46f8bea05eb3ddb9
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-16 17:09:25 +0300

    hourly list item created and parcelable hourly data received from intent

--- app/src/main/java/com/tatar/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/tatar/stormy/ui/HourlyForecastActivity.java
@@ -1,15 +1,27 @@
 package com.tatar.stormy.ui;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.tatar.stormy.R;
+import com.tatar.stormy.model.DailyWeather;
+import com.tatar.stormy.model.HourlyWeather;
+
+import java.util.Arrays;
 
 public class HourlyForecastActivity extends AppCompatActivity {
+
+    private HourlyWeather[] hourlyWeathers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        hourlyWeathers = Arrays.copyOf(parcelables, parcelables.length, HourlyWeather[].class);
     }
 }
