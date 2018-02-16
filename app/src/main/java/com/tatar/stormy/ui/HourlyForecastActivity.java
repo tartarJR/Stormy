245b4af461e3298a39e445799759707a0f6a024b
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-16 16:10:34 +0300

    added HourlyForecastActivity and its related

---
+++ app/src/main/java/com/tatar/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,15 @@
+package com.tatar.stormy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tatar.stormy.R;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+    }
+}
